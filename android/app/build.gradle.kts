d276be2d9a968c95e32cc8ec7ef54a3063eb443b
Hero <126396112+muhammadlai@users.noreply.github.com>
2026-09-12 23:49:06 +0500

    Bump SARA Android to 1.3.0 microphone build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sara.creator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.3.0"
     }
 }
 
