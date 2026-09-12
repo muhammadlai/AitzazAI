ffb28717f1d64121907a382f64e76071d2fa8ee0
Hero <126396112+muhammadlai@users.noreply.github.com>
2026-09-12 10:19:25 +0500

    Bump SARA Android to mic-fix v1.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,10 +11,9 @@ android {
         applicationId = "com.sara.creator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 }
 
 kotlin { jvmToolchain(17) }
-
